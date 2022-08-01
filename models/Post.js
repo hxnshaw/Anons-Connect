719b65ee925378e90afd276d295474a86b977e43
hxnshaw <eghosasere07@gmail.com>
2022-08-01 23:43:59 +0100

    Schema update

--- models/Post.js
+++ models/Post.js
@@ -4,18 +4,27 @@ const PostSchema = new mongoose.Schema(
   {
     heading: {
       type: String,
-      required: true,
+      required: [true, "DO NOT LEAVE BLANK!"],
       trim: true,
       maxlength: 100,
     },
     body: {
       type: String,
-      required: true,
+      required: [true, "REQUIRED!"],
       trim: true,
       maxlength: 1000,
     },
+    image: {
+      type: String,
+      default: "/connect/image.jpeg",
+    },
+    anon: {
+      type: mongoose.Schema.ObjectId,
+      required: true,
+      ref: "User",
+    },
   },
-  { timestamps: true }
+  { timestamps: true, toJSON: { virtuals: true }, toObject: { virtuals: true } }
 );
 
 module.exports = mongoose.model("Post", PostSchema);

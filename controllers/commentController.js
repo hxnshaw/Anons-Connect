deeb7312948a05f71744b8104a1bc12749e86c05
hxnshaw <eghosasere07@gmail.com>
2022-08-05 23:54:20 +0100

    Delete comment

--- controllers/commentController.js
+++ controllers/commentController.js
@@ -2,6 +2,7 @@ const Comment = require("../models/Comment");
 const { StatusCodes } = require("http-status-codes");
 const CustomError = require("../errors");
 const Post = require("../models/Post");
+const checkPermissions = require("../utils/checkPermissions");
 
 const createComment = async (req, res) => {
   const { post: postId } = req.body;
@@ -39,7 +40,16 @@ const getAllComments = async (req, res) => {
 };
 
 const deleteComment = async (req, res) => {
-  res.send("Delete a comment");
+  const { id: commentId } = req.params;
+  const userComment = await Comment.findOne({ _id: commentId });
+  if (!userComment) {
+    throw new CustomError.NotFoundError(`NO COMMENT WITH ID ${commentId} `);
+  }
+
+  checkPermissions(req.user, userComment.user);
+
+  await userComment.remove();
+  res.status(StatusCodes.OK).json({ message: "COMMENT DELETED" });
 };
 
 module.exports = {
